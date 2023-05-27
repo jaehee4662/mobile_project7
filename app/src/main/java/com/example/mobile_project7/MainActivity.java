f85480310904fddb311197eb43d7e36a25913959
jaehee4662-git <hanjaehee0603@gmail.com>
2023-05-27 19:11:30 +0900

    2nd upload

--- app/src/main/java/com/example/mobile_project7/MainActivity.java
+++ app/src/main/java/com/example/mobile_project7/MainActivity.java
@@ -23,18 +23,25 @@ public class MainActivity extends AppCompatActivity {
         tv1 = (TextView) findViewById(R.id.tv1);
         tv2 = (TextView) findViewById(R.id.tv2);
 
-        String fullText = "모바일 프로그래밍 7조";
-        SpannableString spannableString = new SpannableString(fullText);
+        String fullText1 = "모바일 프로그래밍 7조";
+        SpannableString spannableString1 = new SpannableString(fullText1);
+        String fullText2 = "부산광역시 버스 정보 앱";
+        SpannableString spannableString2 = new SpannableString(fullText2);
 
+        int startIndex1 = 4; // 시작 인덱스
+        int endIndex1 = 9; // 끝 인덱스
+        String colorHex1 = "#146C94"; // 변경할 색상을 16진수로 지정
+        int color1 = Color.parseColor(colorHex1); // 16진수 색상을 int로 변환
+        spannableString1.setSpan(new ForegroundColorSpan(color1), startIndex1, endIndex1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
 
+        int startIndex2 = 0; // 시작 인덱스
+        int endIndex2 = 5; // 끝 인덱스
+        String colorHex2 = "#146C94"; // 변경할 색상을 16진수로 지정
+        int color2 = Color.parseColor(colorHex2); // 16진수 색상을 int로 변환
+        spannableString2.setSpan(new ForegroundColorSpan(color2), startIndex2, endIndex2, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
 
-        int startIndex = 8; // 시작 인덱스
-        int endIndex = 10; // 끝 인덱스
-        String colorHex = "#146C94"; // 변경할 색상을 16진수로 지정
-        int color = Color.parseColor(colorHex); // 16진수 색상을 int로 변환
-        spannableString.setSpan(new ForegroundColorSpan(color), startIndex, endIndex, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-
-        tv1.setText(spannableString);
+        tv1.setText(spannableString1);
+        tv2.setText(spannableString2);
 
     }
 
